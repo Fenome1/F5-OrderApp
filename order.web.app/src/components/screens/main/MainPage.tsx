8bab3e702347f0d43b4e39e74fc393d4c9fefdc0
Fenome1 <warfaceserii@mail.ru>
2024-03-14 13:18:55 +0300

    public assets refactor

--- order.web.app/src/components/screens/main/MainPage.tsx
+++ order.web.app/src/components/screens/main/MainPage.tsx
@@ -12,41 +12,41 @@ const duties = [
     {
         id: 1,
         title: 'Концепция сайта',
-        image: "src/assets/conteption.jpg",
+        image: "public/Images/conteption.jpg",
         description:
             'Чтобы создать качественный продукт, мы определяем цели проекта и разрабатываем стратегию их достижения. На этом этапе мы выстраиваем эффективную коммуникацию с целевой аудиторией.',
     },
     {
         id: 2,
         title: 'Дизайн сайта',
-        image: "src/assets/design.jpg",
+        image: "public/Images/design.jpg",
         description:
             'Вдохновляясь лучшими мировыми примерами, мы разрабатываем уникальный визуальный стиль для компании. Учитываем потребности аудитории сайта, заботимся об удобстве использования.',
     },
     {
         id: 3,
         title: 'Разработка сайта',
-        image: "src/assets/code.jpg",
+        image: "public/Images/code.jpg",
         description:
             'HTML-верстка, интеграция с системой управления сайтом, тестирование, первичное наполнение информацией.',
     },
     {
         id: 4,
         title: 'Продвижение',
-        image: "src/assets/chart.jpg",
+        image: "public/Images/chart.jpg",
         description:
             'Ваша целевая аудитория обязательно должна узнать о вашем новом сайте. Мы поможем осуществить грамотное продвижение по многочисленным каналам, чтобы обеспечить привлечение на сайт только целевых клиентов',
     },
     {
         id: 5,
         title: 'Аналитика и маркетинг',
-        image: 'src/assets/analitic.jpg',
+        image: 'public/Images/analitic.jpg',
         description: 'Мы поможем вам понять, кто ваша целевая аудитория, откуда она приходит и как взаимодействует с вашим сайтом. Разработаем эффективные маркетинговые кампании для привлечения новых клиентов и удержания существующих.'
     },
     {
         id: 6,
         title: 'Поддержка и развитие',
-        image: 'src/assets/frend.jpg',
+        image: 'public/Images/frend.jpg',
         description: 'Мы всегда готовы помочь вам с любыми техническими проблемами, связанными с вашим сайтом. Регулярно обновляем сайт, добавляя новые функции и улучшая его производительность.'
     }
 ];
@@ -61,7 +61,7 @@ const MainPage: FC = () => {
         <DefaultPage>
             <Stack className='duties-stack'>
                 <div className='duties-stack-header'>
-                    <img src='../src/assets/monic.png' alt='img' style={
+                    <img src='../public/Images/monic.png' alt='img' style={
                         {
                             filter: `drop-shadow(0 0 0.1rem ${Colors.Fourthly})`
                         }
